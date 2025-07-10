30eeef27c114f72143697ca467bea9d7510a2efd
Narupalle_swapna <nswapna.fullstack@gmail.com>
2025-07-10 23:43:16 +0530

    Add project files.

---
+++ Csharp_step3/methods/1methods.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace MathProgram
+{
+    class Program
+    {
+        //find whether a number is even or odd in a array
+        static string[] ProcessArray(int[] numbers)
+        {
+            //create a string array to store results
+            string[] results = new string[numbers.Length];
+            for(int i=0; i<numbers.Length; i++)
+            {
+                if (numbers[i]%2 == 0)
+                {
+                    results[i] = $"{numbers[i]} is Even";
+                }
+                else
+                {
+                    results[i] = $"{numbers[i]} is Odd";
+                }
+            }
+            return results;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("----check even or odd--------");
+            int[] numbers = { 5, 6, 12, 16, 17, 21 };//input array
+            string[] output = ProcessArray(numbers);
+            Console.WriteLine(output);
+            foreach(string result in output)
+            {
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine("\n-----check Positive/Negative/Zero-----");
+            int[] numbers1 = { -1, 2, 0, 6, 0, -4 };
+            string[] output1 = GetnumberType(numbers1);
+            Console.WriteLine(output1);
+            foreach (string result in output1)
+            {
+                Console.WriteLine(result);
+            }
+        }
+    
+
+   
+    
+        //find whether a number is positive, negative or zero in the array
+        static string[] GetnumberType(int[] numbers1)
+        {
+            string[] results = new string[numbers1.Length];
+            for(int i=0; i<numbers1.Length; i++)
+            {
+                if (numbers1[i] > 0)
+                {
+                    results[i] = $"{numbers1[i]} is positive number";
+                }
+                else if (numbers1[i] < 0)
+                {
+                    results[i] = $"{numbers1[i]} is negative number";
+                }
+                else
+                {
+                    results[i] = $"{numbers1[i]} is zero";
+                }
+            }
+            return results;
+
+        }
+        
+    }
+}
---
+++ Csharp_step3/methods/2methodsAdvanced.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace Methgods_Advanced
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Experience { get; set; }
+        public string Package { get; set; }
+
+    }
+    class AboutEmployee
+    {
+        public static Employee GetEmployee()
+        {
+            Employee employee = new Employee();
+            employee.Name = "Swapna";
+            employee.Experience = 2;
+            employee.Package = "8LPA";
+            return employee;
+        }
+
+        public static Employee UpdateEmployee(Employee employee)
+        {
+            employee.Name = employee.Name + " " + "Narupalle";
+            return employee;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            var singleEmployee = AboutEmployee.GetEmployee();
+            Console.WriteLine($"single employee: {singleEmployee.Name}, {singleEmployee.Experience}, {singleEmployee.Package}");
+
+            var updatedEmployee = AboutEmployee.UpdateEmployee(singleEmployee);
+            Console.WriteLine($"Updated Employee name is: {updatedEmployee.Name}");
+        }
+
+    
+    }
+}
